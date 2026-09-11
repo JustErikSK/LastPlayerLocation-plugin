202369dc6785f7a1bc6eee0cb45eab83d52b7b69
justerik <sebo.erik@gmail.com>
2026-09-11 10:36:39 +0200

    Initial commit

---
+++ src/main/java/net/withrage/lastPlayerLocation/LastPlayerLocation.java
@@ -0,0 +1,16 @@
+package net.withrage.lastPlayerLocation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LastPlayerLocation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
